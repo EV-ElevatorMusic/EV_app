ec1b8782cbcffeaea098664ff7469e498300bc28
Yoonwonwoo <51125312+Yoonwonwoo@users.noreply.github.com>
2019-10-23 22:33:16 +0900

    first commit

--- app/src/main/java/com/example/elevator_music/LoginActivity.java
+++ app/src/main/java/com/example/elevator_music/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.elevator_music;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginText = findViewById(R.id.loginText);
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

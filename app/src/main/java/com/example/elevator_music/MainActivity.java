eb5fe025b4546606dba213fad2fb8d62e00eff12
Yoonwonwoo <51125312+Yoonwonwoo@users.noreply.github.com>
2019-10-23 20:38:22 +0900

    first commit

--- app/src/main/java/com/example/elevator_music/MainActivity.java
+++ app/src/main/java/com/example/elevator_music/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.elevator_music;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }

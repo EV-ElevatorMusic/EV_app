c42dcabac1722339d7deb5259b57894cc3641c93
moseori <haojile96@gmail.com>
2019-10-24 12:32:26 +0900

    email login added

--- app/src/main/java/com/example/elevator_music/LoginActivity.java
+++ app/src/main/java/com/example/elevator_music/LoginActivity.java
@@ -1,28 +1,67 @@
 package com.example.elevator_music;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import javax.security.auth.login.LoginException;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView loginText;
+    EditText et_id, et_password;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        et_id = findViewById(R.id.loginId);
+        et_password = findViewById(R.id.loginPwd);
+        btn_login = findViewById(R.id.loginEnter);
         loginText = findViewById(R.id.loginText);
         loginText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
                 startActivity(intent);
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!et_password.getText().toString().equals("") || !et_id.getText().toString().equals("")) {
+                    auth.signInWithEmailAndPassword(et_id.getText().toString(), et_password.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.e("Login", "onComplete: Success" );
+                                        Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        Log.e("Login", "onComplete: Fail");
+                                        Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else{
+                    Toast.makeText(LoginActivity.this, "빈칸이 있습니다", Toast.LENGTH_SHORT);
+                }
             }
         });
     }

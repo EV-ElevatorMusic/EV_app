6b2e5cf4b78d4f6551180a046eb3de876b6766c9
Yoonwonwoo <51125312+Yoonwonwoo@users.noreply.github.com>
2019-10-28 18:36:27 +0900

    xml 대강 구현

--- app/src/main/java/com/example/elevator_music/RankingActivity.java
+++ app/src/main/java/com/example/elevator_music/RankingActivity.java
@@ -1,14 +1,75 @@
 package com.example.elevator_music;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class RankingActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class RankingActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.nav_view2);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout2);
+        drawer.openDrawer(GravityCompat.END);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout2);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Intent intent;
+
+        if (id == R.id.chatting) {
+            intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.mRanking) {
+            intent = new Intent(this, RankingActivity.class);
+            startActivity(intent);
+        }
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout2);
+        drawer.closeDrawer(GravityCompat.END);
+        return true;
     }
 }

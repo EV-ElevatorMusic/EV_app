d528d881965ff25d79cbddf2cb715b7a2424042b
moseori <haojile96@gmail.com>
2019-11-08 18:14:47 +0900

    LoginIntent

--- app/src/main/java/com/example/elevator_music/RankingActivity.java
+++ app/src/main/java/com/example/elevator_music/RankingActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,15 +15,28 @@ import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+
 public class RankingActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    RecyclerView.Adapter adapterHappy;
+    ArrayList<String> rankingUrl = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
 
+        RecyclerView rvHappy, rvSad, rvMad;
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        rvHappy = findViewById(R.id.musicHappy);
+        rvMad = findViewById(R.id.musicMad);
+        rvSad = findViewById(R.id.musicSad);
+        rankingUrl.add("https://youtu.be/AFSs7MKtpOU");
+        adapterHappy = new RankingHappyAdapter(rankingUrl, getApplicationContext());
+        rvHappy.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        rvHappy.setAdapter(adapterHappy);
+
 
         //NavigationView navigationView = findViewById(R.id.nav_view2);
 
